9913f0baf54b0db4ae9ca5c115b758c31c105da9
youssefokeil <46977560+youssefokeil@users.noreply.github.com>
2024-12-22 20:52:30 +0200

    Update SIGN_EXT.v

--- SIGN_EXT.v
+++ SIGN_EXT.v
@@ -1,12 +1,12 @@
 module SignExtension(
   input wire [15:0] immediate,
-  output reg [31:0] extended,
+  output reg [31:0] extended
 );
   always@(*) begin
     if(immediate[15]==1)
       extended={16'b1111111111111111,immediate[15:0]};
     else
-      extended={16'b0000000000000000,immediate[15:0]};
+      extended={16'b0,immediate[15:0]};
   
   end
 endmodule
