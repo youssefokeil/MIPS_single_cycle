1045d1fb17f33d15f4d916461aad2753ab8c39a0
youssefokeil <46977560+youssefokeil@users.noreply.github.com>
2024-12-22 20:51:39 +0200

    Update INST_MEM.v

--- INST_MEM.v
+++ INST_MEM.v
@@ -1,11 +1,14 @@
 // Code your design here
 module InstructionMemory(
- input [4:0] ReadAddress,
+  input wire [11:0] ReadAddress, //4kB is mapped by 12 bits
  output reg [31:0]  Instruction
 );
   reg [7:0] InstructionMemory [0:4095]; // 4 kB of Instruction Memory
   always @(*) begin
-    Instruction=InstructionMemory[ReadAddress];    
+    Instruction={InstructionMemory[ReadAddress],
+                 InstructionMemory[ReadAddress+1],
+                 InstructionMemory[ReadAddress+2],
+                 InstructionMemory[ReadAddress+3]};    
     end 
   
 endmodule
