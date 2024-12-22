df45554fe07f5e1ec4c17d9e20f11e20fc79fb40
youssefokeil <46977560+youssefokeil@users.noreply.github.com>
2024-12-22 20:52:07 +0200

    Update REGISTER.v

--- REGISTER.v
+++ REGISTER.v
@@ -5,18 +5,21 @@ module Register(
   input [4:0] WriteRegister,
   input [31:0] WriteData,
   input RegWrite,
+  input wire clk,
   
-  output reg ReadData1,
-  output reg ReadData2,
+  output reg[31:0] ReadData1,
+  output reg[31:0] ReadData2
 );
   // register declaration
-  reg [31:0] register [0:31]; // 32 registers each of size 32-bit
+  reg [31:0] register [31:0]; // 32 registers each of size 32-bit
   
   always @(*) begin
     ReadData1 = register[ReadRegister1];
     ReadData2 = register[ReadRegister2];
-    if(RegWrite)
-      register[WriteRegister]=WriteData;      
-      
   end
+  always@(posedge clk) begin
+    if(RegWrite)
+      register[WriteRegister]<=WriteData;
+  end
+    
 endmodule
