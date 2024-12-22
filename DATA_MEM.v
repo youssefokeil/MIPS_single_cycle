757dcf007e135c34aef975ffacfed19a0d29745c
youssefokeil <46977560+youssefokeil@users.noreply.github.com>
2024-12-22 20:51:17 +0200

    Update DATA_MEM.v

--- DATA_MEM.v
+++ DATA_MEM.v
@@ -6,26 +6,28 @@ module Data_Memory #(
   parameter ADDRESS_LENGTH=$clog2(MEMORY_SIZE)
 ) (
    // assume there's no enable for read and write
-  input [DATA_WIDTH-1:0] data_in,
+  input [31:0] data_in,
   input [ADDRESS_LENGTH-1:0] address,
   input inst_type,
+  input wire clk,
   
-  output [DATA_WIDTH-1:0] Data_out,
+  output [31:0] Data_out
 );
   // initialization of the memory
   reg [DATA_WIDTH-1:0] data_memory [0:MEMORY_SIZE-1];
   
   // register for data output
   reg [DATA_WIDTH-1:0] reg_out;
-  always@(Data_in, address, inst_type, data_out)
+  always@(data_in, address, inst_type, Data_out, clk)
     begin
       if(inst_type==0) //lw
         reg_out= {data_memory[address],data_memory[address+1],data_memory[address+2],data_memory[address+3]};
       else //sw
-         {data_memory[address],data_memory[address+1],data_memory[address+2],data_memory[address+3]}=data_in;
+         {data_memory[address],data_memory[address+1],data_memory[address+2],data_memory[address+3]}<=data_in;
           
         
     end
-  assign data_out=reg_out;
+    
+  assign Data_out=reg_out;
   
 endmodule
